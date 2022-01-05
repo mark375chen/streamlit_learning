ab04623018ecac77457f84ffceeacc5f8e93ce79
Mark Chen <mark.chen@goldspot.ca>
2022-01-05 10:48:26 -0500

    clean code

--- src/churn_app.py
+++ src/churn_app.py
@@ -6,38 +6,35 @@ import base64
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def file_download(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  
-    href = f'<a href="data:file/csv;base64,{b64}"
-
-download="churn_data.csv">Download CSV File</a>'
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}"download="churn_data.csv">Download CSV File</a>'
 
     return href
 
+
 def run_app():
+    st.write("""
+# Churn Prediction App
 
-  st.write("""
+Customer churn is defined as the loss of customers after a certain period of time. Companies are interested in targeting customers
 
-  # Churn Prediction App
+who are likely to churn. They can target these customers with special deals and promotions to influence them to stay with
 
-  Customer churn is defined as the loss of customers after a certain period of time. Companies are interested in targeting customers
+the company. 
 
-  who are likely to churn. They can target these customers with special deals and promotions to influence them to stay with
+This app predicts the probability of a customer churning using Telco Customer data. Here
 
-  the company. 
+customer churn means the customer does not make another purchase after a period of time. 
+""")
 
-  This app predicts the probability of a customer churning using Telco Customer data. Here
+    df_selected = pd.read_csv(r'C:\Users\markz\PycharmProjects\streamlit_learning\src\telco_churn.csv')
 
-  customer churn means the customer does not make another purchase after a period of time. 
+    df_selected_all = df_selected[['gender', 'Partner', 'Dependents', 'PhoneService',
+                                   'tenure', 'MonthlyCharges', 'target']].copy()
 
-  """)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
 
-  df_selected = pd.read_csv(r'C:\Users\markz\PycharmProjects\streamlit_learning\src\telco_churn.csv')
-
-  df_selected_all = df_selected[['gender', 'Partner', 'Dependents', 
-  'PhoneService','tenure', 'MonthlyCharges', 'target']].copy()
-  
-  st.set_option('deprecation.showPyplotGlobalUse', False)
-
-  st.markdown(filedownload(df_selected_all), unsafe_allow_html=True)
+    st.markdown(file_download(df_selected_all), unsafe_allow_html=True)
